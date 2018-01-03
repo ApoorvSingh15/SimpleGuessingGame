5a1bc3c5c386d359b88e3eb35abfbca6e73ecb93
ApoorvSingh15 <apoorv.singh15@gmail.com>
2018-01-03 13:56:41 +0530

    js game file added

---
+++ game.js
@@ -0,0 +1,9 @@
+var secNum = 6;
+var guessNum = Number(prompt("Enter a guess number between 1-10"));
+if (guessNum < secNum) {
+	alert("Number is too low");
+}if(guessNum > secNum){
+	alert("Number is too high");
+}if(guessNum === secNum){
+	alert("You got it right");
+}
